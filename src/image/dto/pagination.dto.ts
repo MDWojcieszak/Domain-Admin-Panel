23595ca955cc0d7d99e9112104d0c689b4cce8b7
MDWojcieszak <wojcieszak.4@gmail.com>
2024-02-28 08:38:02 +0100

    remove(image): pagination

--- src/image/dto/pagination.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNumber } from 'nestjs-swagger-dto';
-
-export class PaginationDto {
-  @IsNumber({ min: 1, max: 20 })
-  take: number;
-
-  @IsNumber({ optional: true, default: 0, min: 1 })
-  skip?: number;
-}
