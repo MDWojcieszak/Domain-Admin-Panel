d912bd5e0ea8680f34e78eb69e9017427100dddd
MDWojcieszak <wojcieszak.4@gmail.com>
2024-03-08 16:14:17 +0100

    remove(cronJob): unnecessary jobs

--- src/cron-jobs/cron-jobs.module.ts
+++ src/cron-jobs/cron-jobs.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CronJobsService } from './cron-jobs.service';
-import { MultiVerseModule } from '../multi-verse/multi-verse.module';
 import { ScheduleModule } from '@nestjs/schedule';
-import { ServerService } from '../server/server.service';
 
 @Module({
-  imports: [MultiVerseModule, ScheduleModule.forRoot()],
-  providers: [CronJobsService, ServerService],
+  imports: [ScheduleModule.forRoot()],
+  providers: [CronJobsService],
   exports: [CronJobsService],
 })
 export class CronJobsModule {}
--- src/cron-jobs/cron-jobs.service.ts
+++ src/cron-jobs/cron-jobs.service.ts
@@ -1,32 +1,6 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
-import { Cron, CronExpression } from '@nestjs/schedule';
-import { firstValueFrom } from 'rxjs';
-import { ServerService } from '../server/server.service';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CronJobsService {
-  constructor(
-    @Inject('MULTIVERSE_SERVICE') private multiVerseClient: ClientProxy,
-    private serverService: ServerService,
-  ) {}
-
-  @Cron(CronExpression.EVERY_30_SECONDS)
-  async handleCron() {
-    try {
-      const serverStatus = await firstValueFrom(
-        this.multiVerseClient.send('get_system_usage', {}),
-      );
-
-      this.serverService.updateProperties(serverStatus);
-      if (serverStatus === 'ONLINE') {
-        console.log('Cron job is running because server status is ONLINE');
-        // Perform your cron job logic here
-      } else {
-        console.log('Cron job is skipped because server status is not ONLINE');
-      }
-    } catch (error) {
-      console.error('Error executing cron job:', error);
-    }
-  }
+  constructor() {}
 }
