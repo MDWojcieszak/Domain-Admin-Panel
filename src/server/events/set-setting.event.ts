edc71415e84254718c48a17f8f7d42814ef2495f
MDWojcieszak <wojcieszak.4@gmail.com>
2024-03-16 23:56:32 +0100

    remove(server): dto and events

--- src/server/events/set-setting.event.ts
+++
@@ -1,9 +0,0 @@
-export class SetSettingEvent {
-  constructor(
-    public readonly serverName: string,
-
-    public readonly name: string,
-    public readonly value: string,
-    public readonly category: string,
-  ) {}
-}
