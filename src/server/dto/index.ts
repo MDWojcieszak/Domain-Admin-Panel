bf15fa16fe9d622af6f6a7f9a82e2162aa2d8844
MDWojcieszak <wojcieszak.4@gmail.com>
2024-03-25 23:01:54 +0100

    feature(server): patch disk dto

--- src/server/dto/index.ts
+++ src/server/dto/index.ts
@@ -3,3 +3,4 @@ export * from './process-message.dto';
 export * from './server-properties.dto';
 export * from './register-server.dto';
 export * from './get-server.dto';
+export * from './patch-disk.dto';
---
+++ src/server/dto/patch-disk.dto.ts
@@ -0,0 +1,10 @@
+import { DiskType } from '@prisma/client';
+import { IsEnum, IsString } from 'nestjs-swagger-dto';
+
+export class PatchDiskDto {
+  @IsString({ optional: true })
+  name: string;
+
+  @IsEnum({ enum: { DiskType }, optional: true })
+  mediaType: DiskType;
+}
