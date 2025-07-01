72a436993a7862004f0dbfe53062b291773ad405
MDWojcieszak <wojcieszak.4@gmail.com>
2025-07-01 20:29:36 +0200

    refactor(pagination): update PaginationDto to use optional properties for take and skip

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,13 +1,9 @@
-import { Transform } from 'class-transformer';
 import { IsNumber } from 'nestjs-swagger-dto';
-import { toNumber } from '../../common/helpers/cast.to-number';
 
 export class PaginationDto {
-  @Transform(({ value }) => toNumber(value))
-  @IsNumber({ min: 1, max: 20 })
-  take: number;
+  @IsNumber({ min: 1, max: 20, optional: true })
+  take?: number;
 
-  @Transform(({ value }) => toNumber(value))
   @IsNumber({ optional: true, default: 0, min: 0 })
   skip?: number;
 }
